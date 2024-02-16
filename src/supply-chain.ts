f6757e8dda578312a9fc8d1904bc03f8a7d7e09f
jafetbjarkar <jafetbjarkar@gmail.com>
2024-02-16 08:57:24 +0000

    add another tree graph

--- src/supply-chain.ts
+++ src/supply-chain.ts
@@ -10,11 +10,76 @@ Treegraph(Highcharts);
 
 import { highchartsCommonOptions } from './shared/highcharts-common-options';
 
-/**
- * Tree Graph
- */
-
 document.addEventListener('DOMContentLoaded', () => {
+  /**
+   * Tree Graph - Testing
+   */
+  // @ts-ignore
+  Highcharts.chart(
+    'container3',
+    Highcharts.merge(highchartsCommonOptions, {
+      chart: { spacingBottom: 30, marginRight: 120, height: 140 },
+      series: [
+        {
+          type: 'treegraph',
+          clip: false,
+          keys: ['parent', 'id', 'level'],
+          data: [
+            [undefined, 'Vendor 1'],
+
+            ['Vendor 1', 'Warehouse - West US'],
+            ['Vendor 1', 'Warehouse - East US'],
+
+            ['Warehouse - West US', 'Store 1'],
+            ['Warehouse - West US', 'Store 2'],
+            ['Warehouse - East US', 'Store 3'],
+            ['Warehouse - East US', 'Store 4'],
+
+            // Leaves ( Useful when skipping a level. From Vendor to store for example)
+            // ['Vendor 1', 'Store 1', 3],
+          ],
+          marker: {
+            symbol: 'circle',
+            radius: 6,
+            fillColor: '#ffffff',
+            lineWidth: 3,
+          },
+          dataLabels: {
+            align: 'left',
+            pointFormat: '{point.id}',
+            style: {
+              color: '#000000',
+              textOutline: '3px #ffffff',
+              whiteSpace: 'nowrap',
+              fontWeight: 600,
+            },
+            x: 24,
+            crop: false,
+            overflow: 'none',
+          },
+
+          levels: [
+            { level: 1, color: '#5A25EB' },
+            { level: 2, color: '#29CC80' },
+            { level: 3, color: '#F4D42F' },
+          ],
+          nodes: [
+            { id: 'Vendor 1', color: '#007ad0' },
+            { id: 'Warehouse - West US', color: '#f15c80' },
+            { id: 'Warehouse - East US', color: '#f15c80' },
+            { id: 'Store 1', color: '#f7a35c' },
+            { id: 'Store 2', color: '#f7a35c' },
+            { id: 'Store 3' },
+            { id: 'Store 4' },
+          ],
+        },
+      ],
+    })
+  );
+
+  /**
+   * Tree Graph
+   */
   // @ts-ignore
   Highcharts.chart(
     'container2',
@@ -52,6 +117,7 @@ document.addEventListener('DOMContentLoaded', () => {
               color: '#000000',
               textOutline: '3px #ffffff',
               whiteSpace: 'nowrap',
+              fontWeight: 600,
             },
             x: 24,
             crop: false,
@@ -80,7 +146,6 @@ document.addEventListener('DOMContentLoaded', () => {
   /**
    * Organization chart
    */
-
   // @ts-ignore
   const colChart = Highcharts.chart(
     'container',
@@ -101,7 +166,6 @@ document.addEventListener('DOMContentLoaded', () => {
         {
           type: 'organization',
           name: 'Highsoft',
-
           keys: ['from', 'to'],
           data: [
             ['Vendor', 'Warehouse - West US'],
@@ -113,22 +177,22 @@ document.addEventListener('DOMContentLoaded', () => {
             ['Warehouse - East US', 'Store 3'],
             ['Warehouse - East US', 'Store 4'],
           ],
-          // { label: 'indigo', hex: '#5A25EB', lighter: '#9F81F3', light: '#7C52EF', darker: '#340E9A', dark: '#4313C9' },
-          // { label: 'teal', hex: '#29CC80', lighter: '#77E4B1', light: '#4CDB99', darker: '#18774B', dark: '#20A165' },
-          // { label: 'yellow', hex: '#F4D42F', lighter: '#F9E890', light: '#F7DE5F', darker: '#B39809', dark: '#E4C00C' },
           levels: [
             { level: 0, color: '#d1d0d2' },
             { level: 1, color: '#29CC80' },
             { level: 2, color: '#F4D42F' },
           ],
           nodes: [
-            { id: 'Vendor', color: 'red' },
+            // { id: 'Vendor', color: 'red' },
             // { id: 'HR', title: 'CFO', name: 'Anne Jorunn Fjærestad', color: '#007ad0' },
           ],
           colorByPoint: false,
           color: '#007ad0',
-          dataLabels: { color: '#111111', fontWeight: 'normal' },
-          borderColor: 'white',
+          dataLabels: {
+            color: '#111111',
+            style: { fontWeight: 400, textOutline: '3px #ffffff' },
+          },
+          // borderColor: 'white',
           height: 30,
         },
       ],
